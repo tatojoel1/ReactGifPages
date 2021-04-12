2dc7fa6de0336a389b7c4597280b58b840c0992a
Joel Hernandez <joel92_hernandez@outlook.com>
2021-04-11 21:38:33 -0700

    Adding docs

---
+++ docs/precache-manifest.3f94de865504d1148d2489d1dbb96f51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "301cc7f93de5e38e218f1005e8990115",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bba1b287195a2ec86e42",
+    "url": "/static/css/2.b0704b7a.chunk.css"
+  },
+  {
+    "revision": "a3057cae778752cec814",
+    "url": "/static/css/main.0ac28b19.chunk.css"
+  },
+  {
+    "revision": "bba1b287195a2ec86e42",
+    "url": "/static/js/2.23f8aa7d.chunk.js"
+  },
+  {
+    "revision": "c1bdf306936ddd82567a345834b1f9d5",
+    "url": "/static/js/2.23f8aa7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3057cae778752cec814",
+    "url": "/static/js/main.09cc2c13.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
